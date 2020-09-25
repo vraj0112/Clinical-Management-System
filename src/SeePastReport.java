ede35c0d21b9a2c40c17b5a8e1bf48faeccaf778
uds0128 <68780851+uds0128@users.noreply.github.com>
2020-09-25 21:58:24 +0530

    Update SeePastReport.java

--- src/SeePastReport.java
+++ src/SeePastReport.java
@@ -3,238 +3,167 @@ package clinical.management.system;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.table.DefaultTableModel;
-import java.sql.*;
 
-
-
-public class SeePastReport extends JFrame implements ActionListener
+public class Doctor extends JFrame implements ActionListener
 {
-    JLabel DarkBackGround;
-    JLabel PastReportLabel;
+    ImageIcon Background1;
+    ImageIcon Doctor;
+
+    ImageIcon PatientInfoImage,PastRecordImage,AddReportImage,AppointmentlistImage;
+
+    JButton PatientInfo,PastRecord,AddReport,AppointList;
+    JButton ChangePass, LogOut;
+
+    JLabel PatientInfoLabel,PastRecordLabel,AddReportLabel;
+
+    ImageIcon TempImage;
+    Image temp;
     
-    JLabel TokenNoLabel;
-    JTextField getTokenNo;
-    JButton ShowBT;
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==PatientInfo){
+            new SeePatientInfo().setVisible(true);
+            this.setVisible(false);
+        }else if(ae.getSource()==PastRecord){
+           new SeePastReport().setVisible(true);
+        }else if(ae.getSource()==AddReport){
+            new AddReport().setVisible(true);
+            this.setVisible(false);
+        }else if(ae.getSource()==ChangePass){
+            new DoctorChangePassword().setVisible(true);
+        }else if(ae.getSource()==LogOut){
+            new Login().setVisible(true);
+            this.setVisible(false);
+        }else if(ae.getSource()==AppointList){
+            new AppointmentList().setVisible(true);
+        }
+    }
     
-    JLabel PatientNameLabel,dispNameLabel;
-    
-    JTable PastReportsTB;
-    DefaultTableModel PastReportTableModel;
-    
-    JButton ResetBT,OpenBT;
-    
-    String MobileNumber;
-    
-    public SeePastReport()
-    { 
-        setLayout(null);
-        ImageIcon BlackBackGround = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/black.jpg"));
-        JLabel BackGround = new JLabel(BlackBackGround);
-        BackGround.setBounds(0,0,600,50);
-        add(BackGround);
+    Doctor()
+    {
+
+        Background1 = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/DarkBackGround.png"));
+
+        JLabel LeftPenal = new JLabel(Background1);
+        LeftPenal.setBounds(5, 5, 298, 435);
+        add(LeftPenal);
+
+        TempImage = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/img2.png"));
+        temp = TempImage.getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+        Doctor = new ImageIcon(temp);
+
+        TempImage = null;
+        temp = null;
+
+        JLabel DoctorImage = new JLabel(Doctor);
+        DoctorImage.setBounds(0, 10, 298, 300);
+        LeftPenal.add(DoctorImage);
+
+        ChangePass = new JButton("Change Password");
+        ChangePass.setBounds(5, 445, 298, 55);
+        ChangePass.addActionListener(this);
+        ChangePass.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        add(ChangePass);
+
+        LogOut = new JButton("Log Out");
+        LogOut.setBounds(5, 505, 298, 55);
+        LogOut.addActionListener(this);
+        LogOut.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        add(LogOut);
+
+
+        JLabel Name = new JLabel("Doctor");
+        Name.setBounds(5, 330, 298, 55);
+        Name.setFont(new Font("Times New Roman", Font.PLAIN, 45));
+        Name.setHorizontalAlignment(SwingConstants.CENTER);
+        Name.setForeground(Color.WHITE);
+        LeftPenal.add(Name);
+
+
+        TempImage = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/patientinfo.jpg"));
+        temp = TempImage.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        PatientInfoImage = new ImageIcon(temp);
+
+        TempImage = null;
+        temp = null;
+
+        PatientInfo = new JButton(PatientInfoImage);
+        PatientInfo.addActionListener(this);
+        PatientInfo.setBounds(410, 25, 200, 200);
+        add(PatientInfo);
+
+
+
+        TempImage = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/pastrecords.png"));
+        temp = TempImage.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        PastRecordImage = new ImageIcon(temp);
+
+        TempImage = null;
+        temp = null;
+
+        PastRecord = new JButton(PastRecordImage);
+        PastRecord.addActionListener(this);
+        PastRecord.setBounds(680, 25, 200, 200);
+        add(PastRecord);
+
+
+        TempImage = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/Report&Medicine.png"));
+        temp = TempImage.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        AddReportImage = new ImageIcon(temp);
+
+        TempImage = null;
+        temp = null;
+
+        AddReport = new JButton(AddReportImage);
+        AddReport.addActionListener(this);
+        AddReport.setBounds(410, 300, 200, 200);
+        add(AddReport);
+
+        PatientInfoLabel = new JLabel("Patient Info");
+        PatientInfoLabel.setBounds(410, 200, 200, 100);
+        PatientInfoLabel.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        PatientInfoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        PatientInfoLabel.setForeground(Color.BLACK);
+        add(PatientInfoLabel);
+
+        PastRecordLabel = new JLabel("Past Reports");
+        PastRecordLabel.setBounds(680, 200, 200, 100);
+        PastRecordLabel.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        PastRecordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        PastRecordLabel.setForeground(Color.BLACK);
+        add(PastRecordLabel);
+
+        AddReportLabel = new JLabel("Add New Report");
+        AddReportLabel.setBounds(410, 475, 200, 100);
+        AddReportLabel.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        AddReportLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        AddReportLabel.setForeground(Color.BLACK);
+        add(AddReportLabel);
         
-        PastReportLabel = new JLabel("Past Report");
-        PastReportLabel.setBounds(225,0,150,50);
-        PastReportLabel.setFont(new Font("Times New Roman", Font.PLAIN , 30));
-        PastReportLabel.setForeground(Color.WHITE);
-        BackGround.add(PastReportLabel);
+        TempImage = new ImageIcon(ClassLoader.getSystemResource("clinical/management/system/cms/appont_list.png"));
+        temp = TempImage.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        AppointmentlistImage = new ImageIcon(temp);
+        TempImage = null;
+        temp = null;
         
-        TokenNoLabel = new JLabel("Token No. :");
-        TokenNoLabel.setBounds(95,60,100,30);
-        TokenNoLabel.setFont(new Font("Times New Roman", Font.PLAIN , 18));
-        add(TokenNoLabel);
+        AppointList = new JButton(AppointmentlistImage);
+        AppointList.addActionListener(this);
+        AppointList.setBounds(910,20,50,50);
+        add(AppointList);
         
-        getTokenNo = new JTextField();
-        getTokenNo.setBounds(215, 60, 100, 30);
-        getTokenNo.setFont(new Font("Times New Roman", Font.PLAIN , 18));
-        this.add(getTokenNo);
-        
-        ShowBT = new JButton("Show");
-        ShowBT.setBounds(335,60, 100, 30);
-        ShowBT.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        add(ShowBT);
-        ShowBT.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                        ShowBTactionPerformed(e);
-                    }
-                }
-        );
-        
-        PatientNameLabel = new JLabel("Name :");
-        PatientNameLabel.setBounds(170,100,60,30);
-        PatientNameLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        add(PatientNameLabel);
-        
-        dispNameLabel = new JLabel();
-        dispNameLabel.setBounds(250,100,200,30);
-        dispNameLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        add(dispNameLabel);
-        
-        PastReportsTB = new JTable()
-        {
-            public boolean isCellEditable(int row, int column){
-                return false;
-            } 
-        };
-        
-        PastReportTableModel = (DefaultTableModel)PastReportsTB.getModel();
-        PastReportsTB.setModel(PastReportTableModel);
-        Object []PastReportsTableCloumnModel = {"Time Record"};
-        PastReportTableModel.setColumnIdentifiers(PastReportsTableCloumnModel);
-        PastReportsTB.setFont(new Font("Times New Roman",Font.PLAIN,18));
-                
-        JScrollPane sp = new JScrollPane(PastReportsTB);
-        sp.setBounds(5,150,577,200);
-        add(sp);
-        
-        ResetBT = new JButton("Reset");
-        ResetBT.setBounds(150, 370, 100, 30);
-        ResetBT.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        add(ResetBT);
-        ResetBT.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                        ResetBTactionPerformed(e);
-                    }
-                }
-        );
-        
-        OpenBT = new JButton("Open");
-        OpenBT.setBounds(300, 370, 100, 30);
-        OpenBT.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        add(OpenBT);
-        OpenBT.addActionListener(
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                        OpenBTactionPrformed(e);
-                    }
-                }
-        );
-        
-        this.setSize(600,450);
+        setSize(1000,600);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         
-        this.setVisible(true);
         
-        this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    
-    public void ShowBTactionPerformed(ActionEvent e)
-    {
-        String TokenNo,PatientName;
-        if(getTokenNo.getText().equals(""))
-        {
-            JOptionPane.showMessageDialog(null, "Please Enter Token No...");
-        }
-        else
-        {
-            TokenNo = getTokenNo.getText();
-            try
-            {
-                connection c = new connection();
-                c.createConnection();
-
-                String getMobileNumber = "select * from clinicms.appointment where tokenid = '"+TokenNo+"'";
-                ResultSet rs = c.s.executeQuery(getMobileNumber);
-                rs.next();
-
-                MobileNumber = rs.getString("mobile");
-                PatientName = rs.getString("name");
-                rs = null;
-
-                dispNameLabel.setText(PatientName);
-                
-                String getTimeLine = "select * from patient"+MobileNumber+".time_record";
-                rs = c.s.executeQuery(getTimeLine);
-
-                while(rs.next())
-                {
-                    String Date = "";
-                    Date += rs.getString("dd");
-                    Date += "/";
-                    Date += rs.getString("mm");
-                    Date += "/";
-                    Date += rs.getString("yyyy");
-                    Date += "   ";
-                    Date += rs.getString("hh");
-                    Date += ":";
-                    Date += rs.getString("mt");
-
-                    Object[] Value = {Date};
-
-                    PastReportTableModel.addRow(Value);
-                }
-            }catch(Exception E){}
-        }
-    }
-    
-    public void ResetBTactionPerformed(ActionEvent e)
-    {
-        resetFields();
-    }
-    
-    public void OpenBTactionPrformed(ActionEvent e)
-    {
-        String dd,mm, yyyy ,hh ,mt;
-        if(PastReportsTB.getSelectedRowCount()==1)
-        {
-            int RowToOpen = PastReportsTB.getSelectedRow();
-            String DateToOpen = (String)PastReportTableModel.getValueAt(RowToOpen, 0);
-            dd = DateToOpen.substring(0,2);
-            mm = DateToOpen.substring(3,5);
-            yyyy = DateToOpen.substring(6,10);
-            hh = DateToOpen.substring(13,15);
-            mt = DateToOpen.substring(16,18);
-
-            try
-            {
-                Runtime.getRuntime().exec("rundll32 url.dll, FileProtocolHandler " + "F:\\SGP-1\\Database\\"+MobileNumber+"\\Report\\" + dd +"_"+mm+"_"+yyyy+"_"+hh+"_"+mt+".pdf");
-            }
-            catch(Exception E)
-            {
-                JOptionPane.showMessageDialog(null, "Hekkkp");
-                E.printStackTrace();
-            }
-//            openpdf("F:\\SGP-1\\Database\\"+MobileNumber+"\\Report\\" + dd +"_"+mm+"_"+yyyy+"_"+hh+"_"+mt+".pdf");
-        }
-        else if(PastReportsTB.getSelectedRowCount()>1)
-        {
-            JOptionPane.showMessageDialog(null, "Please Select Only One Report");
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(null, "Please Select Report");
-        }
-    }
-    
-    public static void main(String args[])
-    {
-        new SeePastReport();
-    }
-    
-    public void resetFields()
-    {
-       getTokenNo.setText("");
-       dispNameLabel.setText("");
-       int rowCount = PastReportsTB.getRowCount();
-       for(int i=rowCount-1 ; i>=0 ; i--)
-       {
-           PastReportTableModel.removeRow(i);
-       }
+        setTitle("Doctor");
+        setResizable(false);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void main(String[] args) {
+
+        new Doctor();
     }
-    
 }
